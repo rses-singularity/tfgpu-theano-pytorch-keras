170ad85379ed074f2c2645cf5c0286cc4f403938
Twin <hi@twin.uk.com>
2017-09-12 14:08:51 +0100

    Initial commit, builds tensorflor 1.0.3, theano, keras, pytorch

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+IMG_SIZE=8500
+IMG_NAME="dl-python-gpu"
+
+rm -f $IMG_NAME.img
+sudo singularity create --size $IMG_SIZE $IMG_NAME.img
+sudo singularity bootstrap $IMG_NAME.img Singularity
